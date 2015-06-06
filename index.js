784632875ff7948e8a9a9a14f5164e5457208a33
Mathias Buus <mathiasbuus@gmail.com>
2015-06-06 16:19:42 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var crypto = require('crypto')
+
+var FramedHash = function (algo) {
+  if (!(this instanceof FramedHash)) return new FramedHash(algo)
+  this.hash = crypto.createHash(algo)
+}
+
+FramedHash.prototype.update = function (data) {
+  this.hash.update((typeof data === 'string' ? Buffer.byteLength(data) : data.length) + '\n')
+  this.hash.update(data)
+}
+
+FramedHash.prototype.digest = function (enc) {
+  return this.hash.digest(enc)
+}
+
+module.exports = FramedHash
